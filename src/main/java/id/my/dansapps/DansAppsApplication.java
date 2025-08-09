6df39119ae02fc5eed98d3249c766c89a8f7dead
gramdhanid <gilangramdhan78@gmail.com>
2025-08-09 15:12:25 +0700

    first commit

---
+++ src/main/java/id/my/dansapps/DansAppsApplication.java
@@ -0,0 +1,13 @@
+package id.my.dansapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DansAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DansAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/dansapps/DansAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.dansapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DansAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
